17f69f2d7a01b3f6fed61dac4d4bddc538180592
Parkgeondo <wksehs123@naver.com>
2022-05-02 21:07:07 +0900

    second commit

--- main.js
+++ main.js
@@ -2,13 +2,19 @@ let number = 1;
 
 const principles = [
   {
-    detail:"견고하게 뻩은 스틸 다리와 부드럽게 곡선으로 안아주는 형태의 등받이로 디자인에 전체적인 조화를 더했습니다. 주방,카페,라운지 등 어떤 공간에서도 감각적인 연출을 해보세요."
+    name:"한샘 패브릭 소파"
+    ,detail:"견고하게 뻗은 목제 다리와 부드럽게 곡선으로 안아주는 형태의 등받이로 디자인에 전체적인 조화를 더했습니다. 주방,카페,라운지 등 어떤 공간에서도 감각적인 연출을 해보세요."
+    ,price:"287,000"
   },
   {
-    detail:"부드러운 곡선형 등/좌판 디자인과 감각적인 철제 다리의 조화로 어떤 거실과 매칭을 해도 세련된 분위기를 연출해줍니다. 다리 하부에는 PP캡으로 마감되어 바닥면을 보호하면서 소음을 방지해줍니다."
+    name:"씨엘로 프라페"
+  ,detail:"부드러운 곡선형 등/좌판 디자인과 감각적인 철제 다리의 조화로 어떤 거실과 매칭을 해도 세련된 분위기를 연출해줍니다. 다리 하부에는 PP캡으로 마감되어 바닥면을 보호하면서 소음을 방지해줍니다."
+    ,price:"317,000"
   },
   {
-    detail:"캐시미어처럼 따뜻하고 부드러움 패브릭 마감과 3중 베이스의 소프트 설계 구조로 완성된 포근함으로 하루종일 아늑한 나만의 거실 라이프를 만나보세요."
+    name:"씨엘로"
+    ,detail:"캐시미어처럼 따뜻하고 부드러움 패브릭 마감과 3중 베이스의 소프트 설계 구조로 완성된 포근함으로 하루종일 아늑한 나만의 거실 라이프를 만나보세요."
+    ,price:"407,000"
   }
 ]
 
@@ -17,30 +23,35 @@ const rightTag = document.querySelector("button.right");
 //왼쪽버튼
 const leftTag = document.querySelector("button.left");
 //가구설명
-const furniture = document.querySelector("figure.furniture-image figcaption");
+const furniture_name = document.querySelector("figure.furniture-image figcaption .name");
+//가구설명
+const furniture_detail = document.querySelector("figure.furniture-image figcaption .detail");
+//가구설명
+const furniture_price = document.querySelector("figure.furniture-image figcaption .price");
 //가구 이미지
 const furnitureImage = document.querySelector("figure.furniture-image img");
 //설명창
 const modal = document.querySelector("figcaption.modal");
 //CTA 버튼
 const button = document.querySelector("div.cursor");
-//number_1
-const n1 = document.querySelector("div.number_1");
-//number_2
-const n2 = document.querySelector("div.number_2");
-//number_3
-const n3 = document.querySelector("div.number_3");
+//
+const one = document.querySelector("div.carousel");
+
+const box_1 = document.querySelector("div.box_1");
+const box_2 = document.querySelector("div.box_2");
+const box_3 = document.querySelector("div.box_3");
 
 
 rightTag.addEventListener("click", function() {
   number++;
-
   if(number >= 4){
     number = 1;
   }
 
   furnitureImage.src = `pngwing_${number}.png`
-  furniture.innerHTML = principles[number-1].detail;
+  furniture_detail.innerHTML = principles[number-1].detail;
+  furniture_name.innerHTML = principles[number-1].name;
+  furniture_price.innerHTML = principles[number-1].price;
   modal.style.display = 'none';
 });
 
@@ -51,18 +62,19 @@ leftTag.addEventListener("click", function() {
   if(number <= 0){
     number = 3;
   }
+
   furnitureImage.src = `pngwing_${number}.png`
-  furniture.innerHTML = principles[number-1].detail;
+  furniture_detail.innerHTML = principles[number-1].detail;
+  furniture_name.innerHTML = principles[number-1].name;
+  furniture_price.innerHTML = principles[number-1].price;
   modal.style.display = 'none';
 });
 
 button.addEventListener("click", function(){
   modal.style.display = 'block';
+
 });
 
-button.addEventListener("click", function(){
-  modal.style.display = 'block';
-});
 
 
 var canvas;
@@ -130,3 +142,22 @@ function draw() {
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+//물건 옆에 버튼 - 트랜스레이트 조절해서 움직이는 원리
+let index = 1;
+rightTag.addEventListener('click', () => { 
+  index++;
+
+  if(index >= 4){
+    index = 1;
+  }
+    one.style.transform = `translateY(${26* (index-1)}px)`;
+}); 
+leftTag.addEventListener('click', () => { 
+  index--;
+
+  if(index <= 0){
+    index = 3;
+  }
+    one.style.transform = `translateY(${26* (index-1)}px)`;
+});
