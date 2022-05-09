9a40bf711961246fdb3f409f547ab9eed4e4b4fd
Parkgeondo <wksehs123@naver.com>
2022-05-09 22:51:45 +0900

    seconde commit

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const layers = document.querySelectorAll(".move");
 const darkMode = document.querySelectorAll(".dark");
 const darkModeWrap = document.querySelector(".dark_wrap");
 const darkIcon = document.querySelector(".icon3");
+const aside = document.querySelector(".aside");
+const Menu = document.querySelector(".Menu");
+const Menu_one = document.querySelector(".Menu_one");
+const Menu_two = document.querySelector(".Menu_two");
 
 wrapLayer.addEventListener("mouseover", function(){
     for(let i = 0; i < layers.length; i++){
@@ -29,5 +33,12 @@ darkModeWrap.addEventListener("click", function(){
         console.log("close")
         darkIcon.innerHTML = '<img src="sun.svg">'
     }
-    
 })
+
+Menu.addEventListener("click", function(){
+     aside.classList.toggle("open");
+     Menu.classList.toggle("open");    
+     Menu_one.classList.toggle("open");    
+     Menu_two.classList.toggle("open");    
+})
+
